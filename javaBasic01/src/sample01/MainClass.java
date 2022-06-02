ee8d384a2e4aa84de624d996e4601ef299d799f0
user <user@LAPTOP-3F44AHM6>
2022-06-02 18:07:48 +0900

    java 기본 문법 정리

---
+++ javaBasic01/src/sample01/MainClass.java
@@ -0,0 +1,21 @@
+package sample01;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * 숫자(상수) : 2진수 0~1
+		 * 			 8진수 0~7, 9 -> 011 
+		 * 			 10진수 0~9
+		 * 			 16진수 0~9, A B ~ F(15)
+		 * 
+		 */
+		
+		char arr[] = {'a', 'b', 'c'};
+		
+		
+	}
+
+}
