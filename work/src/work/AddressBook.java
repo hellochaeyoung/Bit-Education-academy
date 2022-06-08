8019ae23b6492aa6a64669359af39bdcc0bb51c7
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-06-08 17:04:16 +0900

    문제 풀이 : 전화번호부 구현하기 - 배열 출력 방법 추가

--- work/src/work/AddressBook.java
+++ work/src/work/AddressBook.java
@@ -8,6 +8,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
@@ -37,7 +38,7 @@ public class AddressBook {
 		
 		String[][] phoneBook = new String[100][5];
 		
-		getPhoneData(phoneBook);
+		load(phoneBook);
 		
 		while(true) {
 			
@@ -50,7 +51,7 @@ public class AddressBook {
 			
 			switch(n) {
 				case 1 : {
-					add(phoneBook);
+					insert(phoneBook);
 					break;
 				}
 				case 2 : {
@@ -58,7 +59,7 @@ public class AddressBook {
 					break;
 				}
 				case 3 : {
-					find(phoneBook);
+					select(phoneBook);
 					break;
 				}
 				case 4 : {
@@ -83,7 +84,7 @@ public class AddressBook {
 
 	}
 	
-	static void getPhoneData(String[][] phoneBook) throws Exception {
+	static void load(String[][] phoneBook) throws Exception {
 		
 		File file = new File("c:\\Temp\\phoneBook.txt");
 		
@@ -93,7 +94,7 @@ public class AddressBook {
 			String str = "";
 			
 			while((str = br.readLine()) != null) {
-				st = new StringTokenizer(str);
+				st = new StringTokenizer(str, "/");
 				for(int i=0;i<5;i++) {
 					phoneBook[cnt][i] = st.nextToken();
 				}
@@ -103,7 +104,10 @@ public class AddressBook {
 		
 	}
 	
-	static void add(String[][] phoneBook) throws IOException {
+	// CRUD - Create Read Update Delete
+	// DAO - Data Access Object
+	
+	static void insert(String[][] phoneBook) throws IOException {
 		
 		System.out.println("추가할 정보를 입력하세요(이름/나이/전화번호/주소/메모)=>");
 		
@@ -122,80 +126,107 @@ public class AddressBook {
 	
 	static void delete(String[][] phoneBook) throws IOException {
 		
-		System.out.println("삭제할 번호의 이름을 입력하세요=>");
+		int idx = search(phoneBook);
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String name = br.readLine();
-		
-		boolean check = false;
-		for(int i=0;i<cnt;i++) {
-			if(phoneBook[i][0].equals(name)) {
-				check = true;
-				for(int j=0;j<5;j++) {
-					phoneBook[i][j] = null;
-				}
-				break;
-			}
+		if(idx == -1) {
+			System.out.println("해당 이름의 정보를 찾을 수 없습니다.");
+			return;
 		}
 		
-		if(check) System.out.println("삭제를 완료하였습니다.");
-		else System.out.println("해당 이름을 찾을 수 없습니다.");
+		for(int j=0;j<5;j++) {
+			phoneBook[idx][j] = null;
+		}
+		System.out.println("삭제를 완료하였습니다.");
+		
 	
 	}
 	
-	static void find(String[][] phoneBook) throws IOException {
+	static void select(String[][] phoneBook) throws IOException {
 		
-		System.out.println("검색할 번호의 이름을 입력하세요=>");
-		 
-		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		 String name = br.readLine();
-		 
-		 boolean check = false;
-			for(int i=0;i<cnt;i++) {
-				if(phoneBook[i][0].equals(name)) {
-					check = true;
-					for(int j=0;j<5;j++) {
-						System.out.print(phoneBook[i][j] + " ");
-					}
-					System.out.println();
-					break;
-				}
-			}
-		 
-		if(check) System.out.println("조회를 완료하였습니다.");
-		else System.out.println("해당 이름의 정보를 찾을 수 없습니다.");
+		String[][] findMember = searchAll(phoneBook);
+		
+		if(findMember == null) {
+			System.out.println("해당 이름의 정보를 찾을 수 없습니다.");
+			return;
+		}
+		
+		for(int j=0;j<5;j++) {
+			System.out.println(Arrays.toString(findMember[j]));
+		}
+		
 	}
 	
 	static void update(String[][] phoneBook) throws IOException {
 		
-		System.out.println("수정할 정보의 이름을 입력하세요=>");
+		int idx = search(phoneBook);
+		
+		if(idx == -1) {
+			System.out.println("해당 이름의 정보를 찾을 수 없습니다.");
+			return;
+		}
+		
+		System.out.println("수정할 번호와 내용을 입력하세요(1.이름, 2.나이, 3.번호, 4.주소, 5.메모), 형식 : 번호/내용");
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), "/");
+		
+		int n = Integer.parseInt(st.nextToken());
+		String newInfo = st.nextToken();
+		phoneBook[idx][n-1] = newInfo;
+		
+		System.out.println("수정을 완료하였습니다.");
+		
+	}
+	
+	static int search(String[][] phoneBook) throws IOException {
+		
+		System.out.println("이름을 입력하세요=>");
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String name = br.readLine();
-		 
-		boolean check = false;
+		
 		for(int i=0;i<cnt;i++) {
 			if(phoneBook[i][0].equals(name)) {
-				check = true;
-				System.out.println("수정할 번호와 변경 내용을 입력하세요(1.이름, 2.나이, 3.전화번호, 4.주소, 5.메모), 형식은 [번호/내용]=>");
-				
-				StringTokenizer st = new StringTokenizer(br.readLine(), "/");
-				
-				int n = Integer.parseInt(st.nextToken());
-				String newInfo = st.nextToken();
-				phoneBook[i][n-1] = newInfo;
-				
-				break;
+				return i;
 			}
 		}
 		
-		if(check) System.out.println("수정을 완료하였습니다.");
-		else System.out.println("해당 이름의 정보를 찾을 수 없습니다.");
+		return -1;
+	}
+	
+	// 동명이인 처리
+	static String[][] searchAll(String[][] phoneBook) throws IOException {
+		
+		System.out.println("이름을 입력하세요=>");
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String name = br.readLine();
+		
+		int count = 0;
+		for(int i=0;i<cnt;i++) {
+			if(phoneBook[i][0] != null && phoneBook[i][0].equals(name)) { // null 체크 꼭 필요!! 또는 name.equals(phoneBook[i][0]) 이렇게 순서를 바꿔주는 게 더 좋다(NPE 방지위해)
+				count++;
+			}
+		}
+		
+		if(count == 0) return null;
+		
+		String[][] resultArray = new String[count][5];
+		int idx = 0;
+		for(int i=0;i<cnt;i++) {
+			if(phoneBook[i][0] != null && phoneBook[i][0].equals(name)) {
+				resultArray[idx] = phoneBook[i];
+				idx++;
+			}
+		}
+		
+		return resultArray;
 	}
 	
 	
 	static void print(String[][] phoneBook) {
 		
+		/*
 		for(int i=0;i<cnt;i++) {
 			boolean check = true;
 			for(int j=0;j<5;j++) {
@@ -207,10 +238,19 @@ public class AddressBook {
 			}
 			if(check) System.out.println();
 			
+		}*/
+		
+		for (int i = 0; i < phoneBook.length; i++) {
+			if(phoneBook[i][0] != null) {
+				System.out.println(Arrays.toString(phoneBook[i]));
+			}
+			
 		}
 	}
 	
 	static void fileSave(String[][] phoneBook) throws IOException {
+		
+		// TODO : fileSave(String[][] phoneBook)
 		
 		File file = new File("c:\\Temp\\phoneBook.txt");
 		
@@ -222,17 +262,21 @@ public class AddressBook {
 		
 		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(file)));
 		
+		// 1. 데이터 정제 및 정리 후 파일에 쓸 데이터만 따로 배열에 저장하여 쓰는 방법
+		
+		// 2. 반복문 돌면서 데이터 체크하고 바로 파일에 쓰는 방법
 		for(int i=0;i<cnt;i++) {
-			boolean check = true;
+			boolean check = false;
 			StringBuilder sb = new StringBuilder();
+			
 			for(int j=0;j<5;j++) {
-				if(phoneBook[i][j] == null) {
-					check = false;
-					break;
+				if(phoneBook[i] != null) {
+					check = true;
+					sb.append(phoneBook[i][j] + "/");
 				}
-				sb.append(phoneBook[i][j] + " ");
+				
 			}
-			if(check) pw.println(sb.toString().trim());
+			if(check) pw.println(sb.toString());
 			
 		}
 		
