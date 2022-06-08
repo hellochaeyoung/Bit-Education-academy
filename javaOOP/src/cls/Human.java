6d9d67100fff54e8453f07f6c16ba639588f4a7d
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-06-08 17:06:56 +0900

    자바 기본 문법 - OOP

---
+++ javaOOP/src/cls/Human.java
@@ -0,0 +1,16 @@
+package cls;
+
+// 객체 설계
+public class Human {
+	
+	// 멤버 변수 // property
+	public String name;
+	public int age;
+	public String address;
+	
+	// 멤버 함수 => 메소드
+	public void method() { // process
+		System.out.println("Human method()");
+	}
+
+}
