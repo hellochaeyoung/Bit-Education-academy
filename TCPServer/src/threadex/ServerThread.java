fdc50826eff3641e441db63fddefb674fba6d374
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-06-20 21:33:12 +0900

    리팩토링 : for문 -> Lambda, Stream 적용

--- TCPServer/src/threadex/ServerThread.java
+++ TCPServer/src/threadex/ServerThread.java
@@ -5,16 +5,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 import java.util.Set;
+
+import impl.HandlerServerExceptionConsumer;
+import interfaces.HandlerExceptionConsumer;
 
 public class ServerThread extends Thread {
 
 	Socket socket;
 	List<Client> list; // 접속한 또 다른 클라이언트들의 목록
 	Set<String> roomNameSet;
+	
+	HandlerServerExceptionConsumer consumer = new HandlerServerExceptionConsumer();
 	
 	public ServerThread(Socket socket, List<Client> list, Set<String> roomNameSet) {
 		super();
@@ -25,7 +32,7 @@ public class ServerThread extends Thread {
 
 
 	@Override
-	public void run() {
+	public void run(){
 		super.run();
 		
 		try {
@@ -33,7 +40,7 @@ public class ServerThread extends Thread {
 			while(true) {
 				
 				// 수신(receive) 
-				BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream())); 
+				BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 				String str = reader.readLine();
 			
 				System.out.println("client로부터 받은 메시지 : " + str);
@@ -44,6 +51,27 @@ public class ServerThread extends Thread {
 				// 사용자 접속 리스트 처리
 				if(str.startsWith("userInfo")) {
 					String id = str.split("/")[1];
+					
+					/* 1. 람다 함수형 인터페이스 accept 함수 바로 구현하여 사용
+					HandlerExceptionConsumer<Socket, String> consumer = (socket, msg) -> {
+						PrintWriter writer = new PrintWriter(socket.getOutputStream());
+						System.out.println(">>>>>" + msg);
+						writer.println(msg);
+						writer.flush();
+					};*/
+					
+					
+					// 2. 인터페이스 구현체 이용하여 람다식 try-catch 처리
+					list.forEach(c -> {
+						Socket s = c.getSocket();
+						if(s == socket) {
+							c.setId(id);
+						}
+						
+						consumer.accept(s, str);
+					});
+					
+					/* 3. 기존 for문 사용하여 구현
 					for(Client c : list) {
 						if(c.getSocket() == socket) {
 							c.setId(id);
@@ -56,21 +84,30 @@ public class ServerThread extends Thread {
 						writer.flush();
 						
 					}
-					
+					*/
 				}
 				// 채팅방 입장 처리
 				else if(str.startsWith("enter")) {
 					// [enter/방이름/사용자1/사용자2/---]
 					String[] infos = str.split("/");
 					
+					// 방 제목 중복 처리
 					if(!roomNameSet.add(infos[1])) {
 						System.out.println("[roomNameSet]:" + roomNameSet.toString());
-						PrintWriter writer = new PrintWriter(socket.getOutputStream());
-						writer.println("Duplicated Room Name:" + "[" + infos[1] + "]");
-						writer.flush();
+						String msg = "Duplicated Room Name:" + "[" + infos[1] + "]";
+						consumer.accept(socket, msg);
 						continue;
 					}
 					
+					// 각 클라이언트 채팅방 목록에 이름 추가
+					list.forEach(c -> c.getRoomList().add(infos[1]));
+					
+					List<String> userList = Arrays.asList(Arrays.copyOfRange(infos, 2, infos.length));
+					userList.forEach(user -> {
+						list.stream().filter(c -> c.getId().equals(user)).findFirst().ifPresent(u -> consumer.accept(u.getSocket(), str));
+					});
+					
+					/*
 					for(int i=2;i<infos.length;i++) {
 						for(Client c : list) {
 							
@@ -86,7 +123,8 @@ public class ServerThread extends Thread {
 								break;
 							}
 						}
-					}
+						
+					}*/
 					
 				}
 				// 채팅방 메시지 처리
@@ -194,7 +232,22 @@ public class ServerThread extends Thread {
 		
 	}
 	
-	
-	
+	/*
+	private void sendMessage(Socket s, String msg){
+		
+		// Socket s = c.getSocket();
+		PrintWriter writer;
+		try {
+			writer = new PrintWriter(s.getOutputStream());
+			System.out.println(">>>>>" + msg);
+			writer.println(msg);
+			writer.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+	}
+	*/
 	
 }
