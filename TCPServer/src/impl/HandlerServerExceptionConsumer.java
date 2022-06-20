7312fc8cb5c0a8a9149385d583d88b89349266fc
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-06-20 21:32:33 +0900

    람다식 Exception 처리를 위한 try-catch문 적용한 BiConsumer 인터페이스 구현체 클래스

---
+++ TCPServer/src/impl/HandlerServerExceptionConsumer.java
@@ -0,0 +1,27 @@
+package impl;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.function.BiConsumer;
+
+public class HandlerServerExceptionConsumer implements BiConsumer<Socket, String> {
+
+	@Override
+	public void accept(Socket t, String u) {
+		
+		try {
+			PrintWriter writer = new PrintWriter(t.getOutputStream());
+			writer.println(u);
+			writer.flush();
+			System.out.println("[HandlerServerExceptionConsumer]>>>>>" + u);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+
+	
+
+}
---
+++ TCPServer/src/interfaces/HandlerExceptionConsumer.java
@@ -0,0 +1,24 @@
+package interfaces;
+
+import java.io.IOException;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+@FunctionalInterface
+public interface HandlerExceptionConsumer<T, U> {
+
+	void accept(T t, U u) throws Exception;
+	
+	
+	static <T, U> BiConsumer<T, U> handlingBiConsumerWrapper(HandlerExceptionConsumer<T, U> consumer) {
+		
+		return (i, j) -> {
+			try {
+				consumer.accept(i,j);
+			}catch ( Exception e) {
+				throw new RuntimeException(e);
+			}
+		};
+	}
+	
+}
