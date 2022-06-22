74729970cfbc9a22f11bad6f023952c9dc57ded0
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-06-22 17:46:20 +0900

    리팩토링 : forans -> Lambda&Stream 적용

--- GUIClient/src/net/WriteClass.java
+++ GUIClient/src/net/WriteClass.java
@@ -22,145 +22,91 @@ public class WriteClass {
 		this.cf = cf;
 		
 	}
-	
-	public void sendMessage() {
-		
+
+	public void send(String msg) {
+
 		try {
 			PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
-			
-			String msg = "";
-			String id = IdFrame.tf.getText();
-			
-			// 첫 번째 전송
-			if(cf.isFirst) {
-				InetAddress iaddr = socket.getLocalAddress();
-				String ip = iaddr.getHostAddress(); // 첫 번째 전송일 땐 ip를 보낸다.
-				
-				msg = "userInfo/" + id;
-				System.out.println("WriteClass>>>" + msg);
-				
-				
-			}
-			// 그 외 전송
-			else {
-				msg = "[" + id + "]" + cf.textF.getText();
-			}
-			
-			// server로 전송
 			pw.println(msg);
 			pw.flush();
-			
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+	}
+
+	public void sendMessage() {
+
+		String msg = "";
+		String id = IdFrame.tf.getText();
+
+		// 첫 번째 전송
+		if(cf.isFirst) {
+			InetAddress iaddr = socket.getLocalAddress();
+			String ip = iaddr.getHostAddress(); // 첫 번째 전송일 땐 ip를 보낸다.
+
+			msg = "userInfo/" + id;
+			System.out.println("WriteClass>>>" + msg);
+
+
+		}
+		// 그 외 전송
+		else {
+			msg = "[" + id + "]" + cf.textF.getText();
+		}
+
+		send(msg);
 	}
 	
 	public void sendUserJoinMessage(String name) {
-		
-		try {
-			PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
-			
-			String msg = "userInfo/" + name;
-			System.out.println("WriteClass sendUserJoinMessage : " + msg + "@@@@");
-			
-			// server로 전송
-			pw.println(msg);
-			pw.flush();
-			
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+		String msg = "userInfo/" + name;
+		System.out.println("WriteClass sendUserJoinMessage : " + msg + "@@@@");
+
+		send(msg);
 	}
 	
 	// 채팅방 입장 메시지 전송 메소드
 	public void sendEnterChattingRoomMessage(String userNames) {
-		
-		try {
-			PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
-			
-			// server로 전송
-			pw.println(userNames);
-			pw.flush();
-			
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		send(userNames);
 	}
 	
 	// 채팅방 메시지 전송 메소드
 	public void sendRoomChattingMessage(String roomName, String id, String msg) {
-		
-		try {
-			PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
-			
-			// String id = IdFrame.tf.getText();
-			
-			sb = new StringBuilder();
-			sb.append("roomMsg/").append(roomName + "/").append(id + "/").append(msg);
-			
-			// server로 전송
-			pw.println(sb.toString());
-			pw.flush();
-			
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+		sb = new StringBuilder();
+		sb.append("roomMsg/").append(roomName).append("/").append(id).append("/").append(msg);
+
+		send(sb.toString());
 	}
 	
 	// 채팅방 퇴장 메시지 전송 메소드
 	public void sendExitChattingRoomMessage(String roomName, int size) {
-		
-		try {
-			PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
-			
-			String id = IdFrame.tf.getText();
-			
-			sb = new StringBuilder();
-			sb.append("roomExit/").append(roomName + "/").append(id);
-			
-			if(size == 0) {
-				sb.append("/end");
-			}
-			
-			// server로 전송
-			pw.println(sb.toString());
-			pw.flush();
-			
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+		String id = IdFrame.tf.getText();
+
+		sb = new StringBuilder();
+		sb.append("roomExit/").append(roomName).append("/").append(id);
+
+		if(size == 0) {
+			sb.append("/end");
 		}
+
+		send(sb.toString());
+
 	}
 	
 	// 로비 퇴장 메시지 전송 메소드
 	public void sendExitMainMessage() {
-		
-		try {
-			PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
-			
-			String id = IdFrame.tf.getText();
-			
-			sb = new StringBuilder();
-			sb.append("mainExit/").append(id);
-			
-			// server로 전송
-			pw.println(sb.toString());
-			pw.flush();
-			
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+		String id = IdFrame.tf.getText();
+
+		sb = new StringBuilder();
+		sb.append("mainExit/").append(id);
+
+		send(sb.toString());
 		
 	}
 }
